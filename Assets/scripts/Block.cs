854d6322d67d77f40e6debf0a766879a259b3262
paladinze <paladinze@hotmail.com>
2017-10-23 22:23:29 -0400

    added smoke particles for block hit

--- Assets/scripts/Block.cs
+++ Assets/scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour {
 
 	public int actualHits;
 	public int maxHits;
+	public GameObject smokePrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +24,24 @@ public class Block : MonoBehaviour {
 		// destroy the block game object
 		if (actualHits >= maxHits) {
 			Destroy(gameObject);
+			CreateSmoke ();
 		}
 
 
+	}
+
+	void CreateSmoke () {
+		GameObject smoke = Instantiate (smokePrefab, gameObject.transform.position, Quaternion.identity);
+		ParticleSystem.MainModule main = smoke.GetComponent<ParticleSystem> ().main;
+
+		// change the color of the particle to match block color
+		main.startColor = gameObject.GetComponent<SpriteRenderer> ().color;
+
+		// set smoke to appear above blocks
+		smoke.transform.position = new Vector3 (smoke.transform.position.x, smoke.transform.position.y, -3f);
+
+		// set smoke density according to maxhits
+		var emission = smoke.GetComponent<ParticleSystem> ().emission;
+		emission.rateOverTime = 2 * maxHits;
 	}
 }
